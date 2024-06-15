7633690fb6d9e2af3a2fcde6fe5a2a868560df0c
Bruno Oliveira <bruno.deao@gmail.com>
2024-06-15 19:56:36 -0300

    improved user validation in room

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import { createServer, STATUS_CODES } from 'node:http'
 import path from 'node:path'
-import { randomUUID } from 'node:crypto';
+import { randomUUID } from 'node:crypto'
 import express, { NextFunction, Request, Response, json, urlencoded } from 'express'
 import { Server } from 'socket.io'
 import ejs from 'ejs'
+import { hash, compare } from 'bcrypt'
 
 type User = {
   password: string
@@ -20,6 +21,7 @@ const server = createServer(app)
 const io = new Server(server)
 const publicDir = path.join(__dirname, '..', 'public')
 const users: { [username: string]: User } = {}
+const rooms: { [code: string]: { messages: Message[], users: { [username: string]: User } } } = {}
 
 app.use(json())
 app.use(urlencoded({ extended: false }))
@@ -28,8 +30,24 @@ app.set('views', publicDir)
 app.engine('html', ejs.renderFile)
 app.set('view engine', 'html')
 app.get('/', (req: Request, res: Response) => res.render('home.html'))
-app.post('/register', (req: Request, res: Response, next: NextFunction) => {
+app.post('/register', async (req: Request, res: Response, next: NextFunction) => {
   const { username, password } = req.body
+
+  if (!username) {
+    const error = new Error('Missing "username"')
+
+    error['statusCode'] = 400
+
+    return next(error)
+  }
+
+  if (!password) {
+    const error = new Error('Missing "password"')
+
+    error['statusCode'] = 400
+
+    return next(error)
+  }
 
   if (users[username]) {
     const error = new Error('"username" already in use')
@@ -39,12 +57,31 @@ app.post('/register', (req: Request, res: Response, next: NextFunction) => {
     return next(error)
   }
 
-  users[username] = { password, logged: true }
+  const hashedPassword = await hash(password, 10)
 
-  res.redirect(`/${randomUUID()}?u=${username}&p=${password}`)
+  users[username] = { password: hashedPassword, logged: true }
+
+  res.redirect(`/${randomUUID()}?u=${username}&p=${hashedPassword}`)
 })
-app.post('/login', (req: Request, res: Response, next: NextFunction) => {
+app.post('/login', async (req: Request, res: Response, next: NextFunction) => {
   const { username, password } = req.body
+
+  if (!username) {
+    const error = new Error('Missing "username"')
+
+    error['statusCode'] = 400
+
+    return next(error)
+  }
+
+  if (!password) {
+    const error = new Error('Missing "password"')
+
+    error['statusCode'] = 400
+
+    return next(error)
+  }
+
   const user = users[username]
 
   if (!user) {
@@ -55,8 +92,16 @@ app.post('/login', (req: Request, res: Response, next: NextFunction) => {
     return next(error)
   }
 
-  if (user.password !== password) {
+  if (!await compare(password, user.password)) {
     const error = new Error()
+
+    error['statusCode'] = 401
+
+    return next(error)
+  }
+
+  if (user.logged) {
+    const error = new Error('User already logged')
 
     error['statusCode'] = 401
 
@@ -65,13 +110,12 @@ app.post('/login', (req: Request, res: Response, next: NextFunction) => {
 
   user.logged = true
 
-  res.redirect(`/${randomUUID()}?u=${username}&p=${password}`)
+  res.redirect(`/${randomUUID()}?u=${username}&p=${user.password}`)
 })
 app.get('/:code', (req: Request, res: Response, next: NextFunction) => {
   const { code } = req.params
   const { u: username, p: password } = req.query
-  const uuidRegex =
-    /^[0-9a-f]{8}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{12}$/i;
+  const uuidRegex = /^[0-9a-f]{8}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{12}$/i
 
   if (!code || !uuidRegex.test(code)) {
     const error = new Error('Invalid "code"')
@@ -115,6 +159,26 @@ app.get('/:code', (req: Request, res: Response, next: NextFunction) => {
     return next(error)
   }
 
+  if (!user.logged) {
+    const error = new Error('User not logged')
+
+    error['statusCode'] = 403
+
+    return next(error)
+  }
+
+  if (!rooms[code]) rooms[code] = { messages: [], users: {} }
+
+  if (rooms[code].users[<string>username]) {
+    const error = new Error('User is already in room')
+
+    error['statusCode'] = 403
+
+    return next(error)
+  }
+
+  rooms[code].users[<string>username] = user
+
   res.render('chat.html', { username })
 })
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -130,12 +194,12 @@ app.use((error: Error & { statusCode: number }, req: Request, res: Response, nex
   if (!statusCode) statusCode = 500
   if (!message) message = STATUS_CODES[statusCode] ?? 'Internal Server Error'
 
+  console.log(error)
+
   res
     .status(statusCode)
     .send(message)
 })
-
-const rooms: { [code: string]: Message[] } = {}
 
 io.on('connection', socket => {
   const code = socket.handshake.headers.referer?.split('/')[3].split('?')[0]
@@ -144,14 +208,13 @@ io.on('connection', socket => {
 
   if (!code) return console.log('code is empty')
 
-  if (!rooms[code]) rooms[code] = []
-
   socket.join(code)
-  socket.emit('previousMessages', rooms[code])
+
+  if (rooms[code]) socket.emit('previousMessages', rooms[code].messages)
+
   socket
     .on('newMessage', (message: Message) => {
-      console.log({rooms})
-      rooms[code].push(message)
+      rooms[code].messages.push(message)
 
       socket.to(code).emit('receivedMessage', message)
     })
